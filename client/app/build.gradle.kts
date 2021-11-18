45bb0e76f75e832ed74e1eb09b7fe7d53b96211b
Vivek Seshadri <vivek.sesha@gmail.com>
2021-11-18 23:41:19 +0530

    Version update

--- client/app/build.gradle.kts
+++ client/app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
     minSdkVersion(21)
     targetSdkVersion(30)
     multiDexEnabled = true
-    versionCode = 31
+    versionCode = 32
     versionName = "1"
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     vectorDrawables.useSupportLibrary = true
