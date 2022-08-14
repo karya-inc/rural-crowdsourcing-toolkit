47e207020d2b07efaf982125b4ca7136d86ca7e9
Vivek Seshadri <vivek.sesha@gmail.com>
2022-08-14 16:14:55 +0530

    Image labelling now has ability to capture image

--- client/app/src/main/java/com/karyaplatform/karya/ui/scenarios/imageLabelling/ImageLabellingViewModel.kt
+++ client/app/src/main/java/com/karyaplatform/karya/ui/scenarios/imageLabelling/ImageLabellingViewModel.kt
@@ -8,6 +8,7 @@ import com.karyaplatform.karya.data.repo.MicroTaskRepository
 import com.karyaplatform.karya.data.repo.TaskRepository
 import com.karyaplatform.karya.injection.qualifier.FilesDir
 import com.karyaplatform.karya.ui.scenarios.common.BaseMTRendererViewModel
+import com.otaliastudios.cameraview.PictureResult
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -39,6 +40,10 @@ constructor(
   private val _labelState: MutableStateFlow<MutableMap<String, Boolean>> = MutableStateFlow(mutableMapOf())
   val labelState = _labelState.asStateFlow()
 
+  // State of capture result
+  private val _captureResult: MutableStateFlow<PictureResult?> = MutableStateFlow(null)
+  val captureResult = _captureResult.asStateFlow()
+
   /**
    * Complete microtask and move to next
    */
@@ -47,7 +52,10 @@ constructor(
     val labels = JsonObject()
     labelState.value.forEach { (label, state) -> labels.addProperty(label, state) }
     outputData.add("labels", labels)
+    // add output files
+    addOutputFile("image", outputFileParams())
     _labelState.value = mutableMapOf()
+    _captureResult.value = null
     viewModelScope.launch {
       completeAndSaveCurrentMicrotask()
       moveToNextMicrotask()
@@ -91,4 +99,25 @@ constructor(
     _labelState.value = newState
 
   }
+
+  fun setCaptureResult(captureResult: PictureResult) {
+    _captureResult.value = captureResult
+  }
+
+  /**
+   * Get output file params for image
+   */
+  private fun outputFileParams(): Pair<String, String> {
+    val filename = microtaskAssignmentIDs[currentAssignmentIndex]
+    return Pair(filename, "jpg")
+  }
+
+  /**
+   * Output file name
+   */
+  fun outputFilePath(): String {
+    val assignmentId = microtaskAssignmentIDs[currentAssignmentIndex]
+    return assignmentOutputContainer.getAssignmentOutputFilePath(assignmentId, outputFileParams())
+  }
+
 }
