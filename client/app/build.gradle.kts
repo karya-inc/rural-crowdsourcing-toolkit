77d050d970093b09993cc0db30273dd6cf1e1b1e
Vivek Seshadri <visesha@microsoft.com>
2022-08-22 15:29:26 +0530

    Version bump

--- client/app/build.gradle.kts
+++ client/app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
     minSdkVersion(24)
     targetSdkVersion(30)
     multiDexEnabled = true
-    versionCode = 60
+    versionCode = 61
     versionName = "1"
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     vectorDrawables.useSupportLibrary = true
